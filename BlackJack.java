093f0d22450ba21f5812dfc1454ce78f877d4690
huynhstin <justinh800@gmail.com>
2017-01-14 15:35:05 -0800

    Update BlackJack.java
    Added basic split deck

--- BlackJack.java
+++ BlackJack.java
@@ -1,4 +1,3 @@
-
 /**
  * Blackjack Game
  * 
@@ -8,18 +7,25 @@
 import java.util.*;
 public class BlackJack
 {
-    private ArrayList<Card> deck;
-    public static Hand playerOne;
-    public static Hand playerTwo;
+    private ArrayList<Card> deck; //the deck itself
+    public static Hand playerOne; //first player
+    public static Hand firstHalf; //two halves
+    public static Hand secondHalf;
+    public boolean deckSplit;
     
     public BlackJack()
     {
         deck = new ArrayList <Card>();
         fillDeck();
         playerOne = new Hand();
-        playerTwo = new Hand();
+        firstHalf = new Hand();
+        secondHalf = new Hand();
+        deckSplit = false;
     }
     
+    /**
+     * Fill in order
+     */
     public void fillDeck()
     {
         for(int i = 0; i < 13; i++)
@@ -33,11 +39,17 @@ public class BlackJack
         }
     }
     
+    /**
+     * Shuffle
+     */
     public void shuffleDeck()
     {
         Collections.shuffle(deck);
     }
     
+    /**
+     * Get top card of deck
+     */
     public Card drawCard()
     {
         Card topOfDeck = deck.get(0);
@@ -45,6 +57,9 @@ public class BlackJack
         return topOfDeck;
     }
     
+    /**
+     * Add top card of deck to player's hand, remove top card of deck 
+     */
     public void hit(Hand player)
     {
         Card topOfDeck = deck.remove(0);
@@ -54,6 +69,20 @@ public class BlackJack
         check21(player);
     }
     
+    public void deal(Hand player)
+    {
+        for(int i = 0; i < 2; i++) //just hit twice, withour returning current score
+        {
+            Card topOfDeck = deck.remove(0);
+            player.getHand().add(topOfDeck);
+            int cardDrawnVal = topOfDeck.getVal().getValue();
+            player.updateTotal(cardDrawnVal);
+        }
+    }
+    
+    /**
+     * Basic logic for now
+     */
     public void check21(Hand player)
     {
         if(player.getTotal() > 21)
@@ -92,8 +121,44 @@ public class BlackJack
         return deck.iterator();
     }
     
+    /**
+     * Get methods
+     */
     public Hand getPlayer()
     {
         return playerOne;
+    }
+    
+    public Hand getFirstHalf()
+    {
+        return firstHalf;
+    }
+    
+    public Hand getSecondHalf()
+    {
+        return secondHalf;
+    }
+    
+    public boolean isDeckSplit()
+    {
+        return deckSplit;
+    }
+    
+    public void splitDeck(Hand player)
+    {
+        if(player.getHand().size() == 2 && player.getHand().get(0).getVal().values().equals(player.getHand().get(1).getVal().values())) // i'm so sorry 
+        {
+            deckSplit = true;
+            firstHalf.getHand().add(player.getHand().get(0)); 
+            hit(firstHalf);
+            secondHalf.getHand().add(player.getHand().get(1));
+            hit(secondHalf);
+            playerOne.getHand().remove(0); //to make sure that they're not accidentally used anymore
+            playerOne.getHand().remove(1); //take out both the cards
+        }
+        else
+        {
+            System.out.println("You can't do that.");
+        }
     }
 }

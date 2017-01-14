2d04475c9519b0073ae13a2d28fd870d762917b7
huynhstin <justinh800@gmail.com>
2017-01-13 20:37:17 -0800

    Update BlackJack.java

--- BlackJack.java
+++ BlackJack.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class BlackJack
 {
     public Deck deck;
-    public Hand playerOne;
+    public static Hand playerOne;
     public Hand playerTwo;
     public Hand playerOneSplit;
     public Hand playerTwoSplit;
@@ -37,6 +37,44 @@ public class BlackJack
         if(player.getTotal() > 21)
         {
             System.out.println("Bust");
+        }
+        else
+        {
+            System.out.println("Current score: " +player.getTotal());
+        }
+    }
+    
+    public Deck getDeck()
+    {
+        return deck;
+    }
+    
+    public static void main (String [] args)
+    {
+        while(true)
+        {
+            Scanner in = new Scanner(System.in);
+            BlackJack b = new BlackJack();
+            System.out.println("Press [1] to shuffle");
+            System.out.println("Press [2] to view deck");
+            System.out.println("Press [3] to hit");
+            //System.out.println("Press [4] to 
+            int select = in.nextInt();
+            switch(select)
+            {
+                case 1: 
+                     b.getDeck().shuffleDeck();
+                     break;
+                case 2: 
+                     b.getDeck().printDeck();
+                     break;
+                case 3:
+                     b.hit(playerOne);
+                     break;
+                default:
+                     System.out.println("Pick one");
+                     break;
+            }
         }
     }
 }

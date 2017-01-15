d826666cf0782db3b5fb8e99a3a91530c05bdf83
huynhstin <justinh800@gmail.com>
2017-01-14 21:23:04 -0800

    Update CardValue.java
    added strings

--- CardValue.java
+++ CardValue.java
@@ -7,29 +7,35 @@
  */
 public enum CardValue
 {
-    TWO(2),
-    THREE(3), 
-    FOUR(4), 
-    FIVE(5), 
-    SIX(6), 
-    SEVEN(7), 
-    EIGHT(8), 
-    NINE(9), 
-    TEN(10), 
-    JACK(10), 
-    QUEEN(10), 
-    KING(10), 
-    ACE(11);
+    TWO("Two", 2),
+    THREE("Three", 3), 
+    FOUR("Four", 4), 
+    FIVE("Five", 5), 
+    SIX("Six", 6), 
+    SEVEN("Seven", 7), 
+    EIGHT("Eight", 8), 
+    NINE("Nine", 9), 
+    TEN("Ten", 10), 
+    JACK("Jack", 10), 
+    QUEEN("Queen", 10), 
+    KING("King", 10), 
+    ACE("Ace", 11);
     public int cardVal;
-    
-    private CardValue(int num)
+    public String name; 
+    private CardValue(String newName, int num)
     {
+        name = newName;
         cardVal = num;
     }   
     
     public int getValue()
     {
         return cardVal;
+    }
+    
+    public String getString()
+    {
+        return name;
     }
 }
 

3caf0770738a652fb80d607327b1f89c3628e18e
huynhstin <justinh800@gmail.com>
2017-01-24 21:40:27 -0800

    images

--- Card.java
+++ Card.java
@@ -4,16 +4,45 @@
  * @author (Justin Huynh and Aaron Nguyen) 
  * @version (Jan 14 2017)
  */
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
 public class Card
 {
     public CardValue cardVal;
     public Suit suit;
     public boolean visible;
+    private BufferedImage image;
+    private boolean aceSwitched;
     public Card(CardValue v, Suit s)
     {
         cardVal = v;
         suit = s;
         visible = true;
+        aceSwitched = false;
+        try
+        {
+            image = ImageIO.read(new File(cardVal.getString() + "_" + suit.getName() +".jpg"));// use cardval, suit enums to load image
+        }
+        catch(Exception e){}
+    }
+    
+    public BufferedImage getImage()
+    {
+        return image;
+    }
+    
+    public void switchAce()
+    {
+        aceSwitched=true;
+    }
+    
+    public boolean isSwitched()
+    {
+        return aceSwitched;
     }
     
     public Suit getSuit()
